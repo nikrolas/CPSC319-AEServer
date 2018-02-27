abab589c00a9ebc67f593367ec312cf3134f332b
Alan Wong <misteralanwong@gmail.com>
2018-02-27 12:38:05 -0800

    Added CORS configurations
    CORS configuration for staging/production front-end, and localhost

--- src/main/java/com/discovery/channel/Application.java
+++ src/main/java/com/discovery/channel/Application.java
@@ -4,28 +4,48 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 // For WAR deployment
 
-//@SpringBootApplication
-//public class Application extends SpringBootServletInitializer{
-//
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(Application.class);
-//    }
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
-//}
-
-// For local deployment
-
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://staging-aeclient.azurewebsites.net",
+                                "https://staging-aeclient.azurewebsites.net",
+                                "http://aeclient.azurewebsites.net",
+                                "https://aeclient.azurewebsites.net",
+                                "http://localhost:3000",
+                                "http://localhost:5000");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
+
+// For local deployment
+
+//@SpringBootApplication
+//public class Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
