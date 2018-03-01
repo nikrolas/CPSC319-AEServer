9b03f1152b5f731c656e60aad0d1fdb15596db47
Qiushan <aflmjc626@gmail.com>
2018-03-01 10:51:50 -0800

    Update some number patterns in recordtypes table https://piazza.com/class/j9rznx4s44a36c?cid=323

--- sql/toDeploy.sql
+++ sql/toDeploy.sql
@@ -16,3 +16,11 @@ DROP FOREIGN KEY  `FK_RecordClassifications_Records`;
 
 ALTER TABLE recordclassifications
 ADD  CONSTRAINT `FK_RecordClassifications_Records` FOREIGN KEY (`RecordId`) REFERENCES `records` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
+
+# Update recordtypes number pattern based on https://piazza.com/class/j9rznx4s44a36c?cid=323
+UPDATE recordtypes SET NumberPattern='KKK-yyyy/ggg' WHERE Id = 22;
+UPDATE recordtypes SET NumberPattern='KKK-XXXX-XXXX.XXX' WHERE Id = 30;
+UPDATE recordtypes SET NumberPattern='KKK_P_yyyy.ggg' WHERE Id = 32;
+UPDATE recordtypes SET NumberPattern='KKK-TASK-XXXX' WHERE Id = 73;
+UPDATE recordtypes SET NumberPattern='nnnnzzzz.nn.a.nn.nn[:nn]' WHERE Id = 83;
+UPDATE recordtypes SET NumberPattern='KKK-CLIENT.gggg' WHERE Id=70;
