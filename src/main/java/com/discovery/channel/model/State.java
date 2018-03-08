97145f0099615cc2b9bc6a0c300d5f7742560521
Qiushan <aflmjc626@gmail.com>
2018-03-08 12:58:01 -0800

    Endpoints for getting all states

---
+++ src/main/java/com/discovery/channel/model/State.java
@@ -0,0 +1,11 @@
+package com.discovery.channel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class State {
+    int id;
+    String name;
+}
