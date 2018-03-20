b7c9a92b4db9fd68be67b6ed4b7ef3e9453412fb
Shih-Chun Huang <jshihchunhuang@gmail.com>
2018-03-20 07:46:16 +0000

    Update RecordController.java

--- src/main/java/com/discovery/channel/database/RecordController.java
+++ src/main/java/com/discovery/channel/database/RecordController.java
@@ -26,7 +26,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
->>>>>>> src/main/java/com/discovery/channel/database/RecordController.java
 
 
 public class RecordController {
